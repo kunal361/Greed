05d8a45b51bdd6ef312d3c9e053f01b01cd680e3
Kunal Sheth <kunal@Kunals-MacBook-Pro.local>
2015-06-29 20:55:48 +0530

    Fixed indentation

--- DiceSet.rb
+++ DiceSet.rb
@@ -3,10 +3,10 @@ class DiceSet
   attr_reader :values
   
   def roll(number_of_dice)
-  	@values = Array.new # contains random values of the rolled dices which are 'number_of_dice' in number
+    @values = Array.new # contains random values of the rolled dices which are 'number_of_dice' in number
     @values = number_of_dice.times.map do
       rand(6) + 1
     end
   end
-  
+
 end
